8ca73c19466e2cd191a0baf567219411ef959eef
VGoilkar-05 <vaishgoilkar05@gmail.com>
2023-10-26 13:09:34 +0530

    first commit

--- src/Google Meet/Video.jsx
+++ src/Google Meet/Video.jsx
@@ -1,34 +1,3 @@
-// import {useState} from 'react'
-// import GitHub from './GitHub/GitHub'
-// import './GitHub/GitHub.css'
-
-// function App() {
-//   const[data,setData]=useState(GitHub);
-//   return (
-//     <div className='App'>
-      
-//       <section>
-//         <main>
-//           <p>Today Birthday {data.length}🤩  </p>
-//           {data.map((x)=>{
-//           return(
-//             <div className='cards' key={x.image}>
-//               <img src={x.image} alt="" height={'50px'} width={'50px'} ></img>
-//               <ruby><p>{x.age}</p><rt>{x.name}</rt></ruby>
-
-//             </div>
-//               )
-//           })}
-//           <button onClick={()=>setData([])}>clear</button>
-
-//         </main>
-//       </section>
-//     </div>
-//   )
-// }
-
-// export default App
-
 
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
@@ -93,4 +62,3 @@ export default function App() {
     ></div>
   );
 }
-
