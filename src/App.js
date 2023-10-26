0e985a8f3f32e931f102f048599ddc971a9bb1af
VGoilkar-05 <vaishgoilkar05@gmail.com>
2023-10-26 09:41:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import GitHub from './GitHub/GitHub'
+import './GitHub/GitHub.css'
 
 function App() {
+  const[data,setData]=useState(GitHub);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      
+      <section>
+        <main>
+          <p>Today Birthday {data.length}🤩  </p>
+          {data.map((x)=>{
+          return(
+            <div className='cards' key={x.image}>
+              <img src={x.image} alt="" height={'50px'} width={'50px'} ></img>
+              <ruby><p>{x.age}</p><rt>{x.name}</rt></ruby>
+
+            </div>
+              )
+          })}
+          <button onClick={()=>setData([])}>clear</button>
+
+        </main>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/GitHub/GitHub.jsx
@@ -0,0 +1,10 @@
+
+const GitHub=[
+    {id:10,name:"Sharukh Khan",age:57,image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdmRzyZNwMx0EmKTGOvirrf4SjNJosvsCKw&usqp=CAU"},
+    {id:20,name:"Salman Khan",age:45,image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbAOfNQHrP_dTZ1ZF6965YKPl9IeT6zAjnwQ&usqp=CAU"},
+    {id:1,name:"Shahid Kapoor",age:35,image:"https://www.themoviedb.org/t/p/w500/q5jqLRioZgIVjXpKS8XHx27ScDQ.jpg"},
+    {id:2,name:"Alia Bhatt",age:26,image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyVSQfYa5u8YZCUYUQ8Xzv2tNOa-HJywXjxA&usqp=CAU"},
+    {id:3,name:"Vicky Kaushal",age:33,image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDPFVVM4lEXMhVesqJ3sku41sh_t9fbuUmOA&usqp=CAU"},
+    {id:4,name:"Katrina Kaif",age:28,image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQddvkD8WYZ2H9MB9-LfpS_ZRB2UmEMBTHtCA&usqp=CAU"}
+]
+    export default GitHub
